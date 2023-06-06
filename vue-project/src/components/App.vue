3f3e2fcff390ea33d18054b0c8c42045183df6e0
Ismoilov <di.murod.1999@gmail.com>
2023-06-06 14:28:05 +0500

    vue start

---
+++ vue-project/src/components/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="app">
+    <AppInfoVue />
+  </div>
+</template>
+
+<script>
+import AppInfoVue from "@/components/app-info/AppInfo.vue";
+export default {
+  components: {
+    AppInfoVue,
+  },
+};
+</script>
+
+<style>
+.app {
+  background: purple;
+  
+}
+</style>
